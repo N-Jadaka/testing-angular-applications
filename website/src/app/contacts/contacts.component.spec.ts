23d2d982fd9bfddc4a9c03c13399c48e2f616bb1
nezianyajc <jnezia1@hotmail.com>
2021-10-23 14:13:10 -0500

    Set up spec fie for component testing

--- website/src/app/contacts/contacts.component.spec.ts
+++ website/src/app/contacts/contacts.component.spec.ts
@@ -1,46 +1,16 @@
-import ContactClass from './shared/models/contact';
+import { ContactsComponent } from "./contacts.component";
+import { Contact } from './shared/models';
 
-fdescribe('Contact class tests', () => {
-  let contact: ContactClass = null;
-
+fdescribe('ContactsComponents Tests', () => {
+  let contactsComponent: ContactsComponent = null;
   beforeEach(() => {
-    contact = new ContactClass();
+    contactsComponent = new ContactsComponent();
   });
 
-  it('should have a valid constructor', () => {
-    expect(contact).not.toBeNull();
+  it('should set instance correctly', () => {
+    expect(contactsComponent).not.toBeNull();
   });
 
-  it('should set name correctly through constructor', () => {
-    contact = new ContactClass('Josh');
-    expect(contact.name).toEqual('Josh');
-  })
 
-  it('should get and set email correctly', () => {
-    contact.email = 'jnezia1@gmail.com';
-    expect(contact.email).toEqual('jnezia1@gmail.com');
-
-  });
-
-  it('should get and set number', () => {
-    contact.number = '2252259952'
-    expect(contact.number).toEqual('2252259952');
-  });
-
-  it('should get and set country correctly', () => {
-    contact.country = 'United States';
-    expect(contact.country).toEqual('United States');
-  });
-
-  it('should  get and set favorite correctly', () => {
-    contact.favorite = true;
-    expect(contact.favorite).toEqual(true);
-  });
-
-  afterEach(() => {
-    contact = null;
-  });
 
 });
-
-
