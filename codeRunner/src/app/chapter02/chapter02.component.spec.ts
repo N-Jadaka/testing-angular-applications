677fea90d03904dcd080aeec30cbfcb59efa8055
nezianyajc <jnezia1@hotmail.com>
2021-10-23 13:31:32 -0500

    Ran first test

--- codeRunner/src/app/chapter02/chapter02.component.spec.ts
+++ codeRunner/src/app/chapter02/chapter02.component.spec.ts
@@ -19,17 +19,5 @@ fdescribe('Chapter02Component', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
 
-  describe('Chapter 2 tests', () => {
-    it('Our first Jasmine test', () => {
-      expect(true).toBe(true);
-    });
-  });
-
-  it('2 + 2 equals 4', () => {
-    expect(2+2).toBe(4);
-  })
 });
